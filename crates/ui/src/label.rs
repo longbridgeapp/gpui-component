92218734bbc05e7446f300bda5b50225af2b54a2
Floyd Wang <gassnake999@gmail.com>
2024-07-09 15:55:18 +0800

    Add text align property for Label

--- crates/ui/src/label.rs
+++ crates/ui/src/label.rs
@@ -1,12 +1,24 @@
-use gpui::{div, Div, IntoElement, ParentElement, RenderOnce, SharedString, Styled, WindowContext};
+use gpui::{
+    div, prelude::FluentBuilder, Div, IntoElement, ParentElement, RenderOnce, SharedString, Styled,
+    WindowContext,
+};
 
-use crate::theme::ActiveTheme;
+use crate::{h_flex, theme::ActiveTheme};
+
+#[derive(Default)]
+pub enum TextAlign {
+    #[default]
+    Left,
+    Center,
+    Right,
+}
 
 #[derive(IntoElement)]
 pub struct Label {
     base: Div,
     label: SharedString,
     multiple_lines: bool,
+    align: TextAlign,
 }
 
 impl Label {
@@ -15,11 +27,17 @@ impl Label {
             base: div(),
             label: label.into(),
             multiple_lines: true,
+            align: TextAlign::default(),
         }
     }
 
     pub fn multiple_lines(mut self) -> Self {
         self.multiple_lines = true;
+        self
+    }
+
+    pub fn text_align(mut self, align: TextAlign) -> Self {
+        self.align = align;
         self
     }
 }
@@ -38,7 +56,12 @@ impl RenderOnce for Label {
             self.label
         };
 
-        div()
+        h_flex()
+            .map(|this| match self.align {
+                TextAlign::Left => this.justify_start(),
+                TextAlign::Center => this.justify_center(),
+                TextAlign::Right => this.justify_end(),
+            })
             .text_color(cx.theme().foreground)
             .child(self.base.child(text))
     }
