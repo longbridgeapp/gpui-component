c5128d22355125f379baac5726bc835a7550562d
Jason Lee <huacnlee@gmail.com>
2024-07-24 11:32:33 +0800

    Update ScrollView to let `content` is FnOnce.

--- crates/ui/src/scroll/scroll_view.rs
+++ crates/ui/src/scroll/scroll_view.rs
@@ -17,7 +17,7 @@ pub struct ScrollView {
     id: ElementId,
     view: AnyView,
     axix: ScrollbarAxis,
-    content: Option<Box<dyn FnMut(&mut WindowContext) -> AnyElement + 'static>>,
+    content: Option<Box<dyn FnOnce(&mut WindowContext) -> AnyElement + 'static>>,
 }
 
 impl ScrollView {
@@ -50,9 +50,9 @@ impl ScrollView {
 
     /// Set the content render of the scroll view.
     #[must_use]
-    pub fn content<F, E>(mut self, mut builder: F) -> Self
+    pub fn content<F, E>(mut self, builder: F) -> Self
     where
-        F: FnMut(&mut WindowContext) -> E + 'static,
+        F: FnOnce(&mut WindowContext) -> E + 'static,
         E: IntoElement,
     {
         self.content = Some(Box::new(move |cx| builder(cx).into_any_element()));
@@ -125,7 +125,7 @@ impl Element for ScrollView {
         let view = self.view.clone();
 
         let scroll_id = self.id.clone();
-        let content = self.content.as_mut().map(|c| c(cx));
+        let content = self.content.take().map(|c| c(cx));
 
         self.with_element_state(id.unwrap(), cx, |_, element_state, cx| {
             let handle = element_state.handle.clone();
