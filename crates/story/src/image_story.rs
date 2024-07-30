6465969c5aa0c4cd3388c3a724995db45bce545d
Jason Lee <huacnlee@gmail.com>
2024-07-30 19:54:27 +0800

    Improve `svg_img` to use cached assets. (#87)

--- crates/story/src/image_story.rs
+++ crates/story/src/image_story.rs
@@ -1,5 +1,5 @@
 use gpui::{px, ParentElement as _, Render, Styled, View, VisualContext as _, WindowContext};
-use ui::{h_flex, v_flex, SvgImg};
+use ui::{h_flex, svg_img, v_flex, SvgImg};
 
 const GOOGLE_LOGO: &str = include_str!("./fixtures/google.svg");
 const PIE_JSON: &str = include_str!("./fixtures/pie.json");
@@ -7,19 +7,17 @@ const PIE_JSON: &str = include_str!("./fixtures/pie.json");
 pub struct ImageStory {
     google_logo: SvgImg,
     pie_chart: SvgImg,
+    inbox_img: SvgImg,
 }
 
 impl ImageStory {
-    pub fn new(cx: &WindowContext) -> Self {
+    pub fn new(_: &WindowContext) -> Self {
         let chart = charts_rs::PieChart::from_json(PIE_JSON).unwrap();
 
         Self {
-            google_logo: SvgImg::new(800, 800)
-                .svg(GOOGLE_LOGO.as_bytes(), cx)
-                .unwrap(),
-            pie_chart: SvgImg::new(400, 300)
-                .svg(chart.svg().unwrap().as_bytes(), cx)
-                .unwrap(),
+            google_logo: svg_img().source(GOOGLE_LOGO.as_bytes(), px(300.), px(300.)),
+            pie_chart: svg_img().source(chart.svg().unwrap().as_bytes(), px(400.), px(400.)),
+            inbox_img: svg_img().source("icons/inbox.svg", px(300.), px(300.)),
         }
     }
 
@@ -37,12 +35,13 @@ impl Render for ImageStory {
             .child(
                 h_flex()
                     .size_full()
+                    .child(self.google_logo.clone().size(px(300.)).flex_grow())
                     .child(self.google_logo.clone().w(px(300.)).h(px(300.)).flex_grow())
-                    .child(self.google_logo.clone().w(px(300.)).h(px(300.)).flex_grow())
-                    .child(self.google_logo.clone().w(px(300.)).h(px(300.)).flex_grow())
-                    .child(self.google_logo.clone().w(px(300.)).h(px(300.)).flex_grow())
-                    .child(self.google_logo.clone().w(px(300.)).h(px(300.)).flex_grow()),
+                    .child(self.google_logo.clone().size_80().flex_grow())
+                    .child(self.google_logo.clone().size_12().flex_grow())
+                    .child(self.google_logo.clone().w(px(300.)).h(px(300.))),
             )
+            .child(self.inbox_img.clone().w(px(80.)).h(px(80.)))
             .child(self.pie_chart.clone().size_full())
     }
 }
