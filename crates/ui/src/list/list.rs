007c6d54273f072de6229d9da1b9545bcae57948
Jason Lee <huacnlee@gmail.com>
2024-08-02 10:48:59 +0800

    Add debug

--- crates/ui/src/list/list.rs
+++ crates/ui/src/list/list.rs
@@ -199,16 +199,18 @@ where
                 self._search_task = cx.spawn(|this, mut cx| async move {
                     search.await;
 
-                    let _ = this.update(&mut cx, |this, _| {
+                    this.update(&mut cx, |this, _| {
                         this.vertical_scroll_handle.scroll_to_item(0);
                         this.last_query = Some(text);
-                    });
+                    })
+                    .expect("BUG: failed to update list");
 
                     // Always wait 100ms to avoid flicker
                     Timer::after(Duration::from_millis(100)).await;
-                    let _ = this.update(&mut cx, |this, cx| {
+                    this.update(&mut cx, |this, cx| {
                         this.set_loading(false, cx);
-                    });
+                    })
+                    .expect("BUG: failed to update loading status");
                 });
             }
             InputEvent::PressEnter => self.action_confirm(&Confirm, cx),
