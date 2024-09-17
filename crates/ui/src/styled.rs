e0e0fbb780662344fba92c2da6b68b29d028be96
Floyd Wang <gassnake999@gmail.com>
2024-09-17 10:25:43 +0800

    Reduce `popover` border radiu

--- crates/ui/src/styled.rs
+++ crates/ui/src/styled.rs
@@ -126,7 +126,7 @@ pub trait StyledExt: Styled + Sized {
             .border_1()
             .border_color(cx.theme().border)
             .shadow_lg()
-            .rounded_lg()
+            .rounded(px(cx.theme().radius))
     }
 }
 
