6938b90570be9695284fc23c3779ada4830fbc5b
Jason Lee <huacnlee@gmail.com>
2024-12-04 10:49:09 +0800

    input: Add to support muti-line text.

--- crates/ui/src/input/input.rs
+++ crates/ui/src/input/input.rs
@@ -20,10 +20,11 @@ use gpui::{
     ClickEvent, ClipboardItem, Context as _, Element, ElementId, ElementInputHandler, EventEmitter,
     FocusHandle, FocusableView, GlobalElementId, InteractiveElement as _, IntoElement, KeyBinding,
     KeyDownEvent, LayoutId, Model, MouseButton, MouseDownEvent, MouseMoveEvent, MouseUpEvent,
-    PaintQuad, ParentElement as _, Pixels, Point, Render, ShapedLine, SharedString, Style,
+    PaintQuad, ParentElement as _, Pixels, Point, Rems, Render, ShapedLine, SharedString, Style,
     Styled as _, TextRun, UTF16Selection, UnderlineStyle, View, ViewContext, ViewInputHandler,
-    WindowContext,
+    WindowContext, WrappedLine,
 };
+use smallvec::SmallVec;
 use unicode_segmentation::*;
 
 actions!(
@@ -120,6 +121,7 @@ pub fn init(cx: &mut AppContext) {
 pub struct TextInput {
     focus_handle: FocusHandle,
     text: SharedString,
+    multi_line: bool,
     history: History<Change>,
     blink_cursor: Model<BlinkCursor>,
     prefix: Option<Box<dyn Fn(&mut ViewContext<Self>) -> AnyElement + 'static>>,
@@ -154,6 +156,7 @@ impl TextInput {
         let input = Self {
             focus_handle: focus_handle.clone(),
             text: "".into(),
+            multi_line: false,
             blink_cursor,
             history,
             placeholder: "".into(),
@@ -198,6 +201,12 @@ impl TextInput {
         cx.on_blur(&focus_handle, Self::on_blur).detach();
 
         input
+    }
+
+    /// Use the text input field as a multi-line Textarea.
+    pub fn multi_line(mut self) -> Self {
+        self.multi_line = true;
+        self
     }
 
     /// Set the text of the input field.
@@ -882,6 +891,7 @@ impl TextElement {
 struct PrepaintState {
     scroll_offset: Point<Pixels>,
     line: Option<ShapedLine>,
+    lines: Option<SmallVec<[WrappedLine; 1]>>,
     cursor: Option<PaintQuad>,
     selection: Option<PaintQuad>,
     bounds: Bounds<Pixels>,
@@ -910,7 +920,11 @@ impl Element for TextElement {
     ) -> (LayoutId, Self::RequestLayoutState) {
         let mut style = Style::default();
         style.size.width = relative(1.).into();
-        style.size.height = cx.line_height().into();
+        style.size.height = if self.input.read(cx).multi_line {
+            (10. * cx.line_height()).into()
+        } else {
+            cx.line_height().into()
+        };
         (cx.request_layout(style, []), ())
     }
 
@@ -921,11 +935,12 @@ impl Element for TextElement {
         _request_layout: &mut Self::RequestLayoutState,
         cx: &mut WindowContext,
     ) -> Self::PrepaintState {
+        let multi_line = self.input.read(cx).multi_line;
         let input = self.input.read(cx);
         let text = input.text.clone();
         let placeholder = input.placeholder.clone();
         let selected_range = input.selected_range.clone();
-        let cursor = input.cursor_offset();
+        let cursor_offset = input.cursor_offset();
         let style = cx.text_style();
 
         let (display_text, text_color) = if text.is_empty() {
@@ -976,64 +991,75 @@ impl Element for TextElement {
         };
 
         let font_size = style.font_size.to_pixels(cx.rem_size());
-        let line = cx
-            .text_system()
-            .shape_line(display_text, font_size, &runs)
-            .unwrap();
+        let mut line = None;
+        let mut lines = None;
+        if multi_line {
+            let wrap_width = bounds.size.width;
+            lines = cx
+                .text_system()
+                .shape_text(display_text, font_size, &runs, Some(wrap_width))
+                .ok();
+        } else {
+            line = cx
+                .text_system()
+                .shape_line(display_text, font_size, &runs)
+                .ok()
+        };
 
         // Calculate the scroll offset to keep the cursor in view
         let mut scroll_offset = input.scroll_offset;
         let mut bounds = bounds;
-        let right_margin = px(5.);
-        let cursor_pos = line.x_for_index(cursor);
-        let cursor_start = line.x_for_index(selected_range.start);
-        let cursor_end = line.x_for_index(selected_range.end);
-        let cursor_moved = input.last_cursor_offset != Some(cursor);
-        let selection_changed = input.last_selected_range != Some(selected_range.clone());
+        let mut selection = None;
+        let mut cursor = None;
+        const RIGHT_MARGIN: Pixels = px(5.);
+        const INSET: Pixels = px(0.5);
 
-        if cursor_moved || selection_changed {
-            scroll_offset.x = if scroll_offset.x + cursor_pos > (bounds.size.width - right_margin) {
-                // cursor is out of right
-                bounds.size.width - right_margin - cursor_pos
-            } else if scroll_offset.x + cursor_pos < px(0.) {
-                // cursor is out of left
-                scroll_offset.x - cursor_pos
-            } else {
-                scroll_offset.x
-            };
+        if let Some(line) = &line {
+            let cursor_pos = line.x_for_index(cursor_offset);
+            let cursor_start = line.x_for_index(selected_range.start);
+            let cursor_end = line.x_for_index(selected_range.end);
+            let cursor_moved = input.last_cursor_offset != Some(cursor_offset);
+            let selection_changed = input.last_selected_range != Some(selected_range.clone());
 
-            if input.selection_reversed {
-                if scroll_offset.x + cursor_start < px(0.) {
-                    // selection start is out of left
-                    scroll_offset.x = -cursor_start;
-                }
-            } else {
-                if scroll_offset.x + cursor_end <= px(0.) {
-                    // selection end is out of left
-                    scroll_offset.x = -cursor_end;
+            if cursor_moved || selection_changed {
+                scroll_offset.x =
+                    if scroll_offset.x + cursor_pos > (bounds.size.width - RIGHT_MARGIN) {
+                        // cursor is out of right
+                        bounds.size.width - RIGHT_MARGIN - cursor_pos
+                    } else if scroll_offset.x + cursor_pos < px(0.) {
+                        // cursor is out of left
+                        scroll_offset.x - cursor_pos
+                    } else {
+                        scroll_offset.x
+                    };
+
+                if input.selection_reversed {
+                    if scroll_offset.x + cursor_start < px(0.) {
+                        // selection start is out of left
+                        scroll_offset.x = -cursor_start;
+                    }
+                } else {
+                    if scroll_offset.x + cursor_end <= px(0.) {
+                        // selection end is out of left
+                        scroll_offset.x = -cursor_end;
+                    }
                 }
             }
-        }
 
-        bounds.origin = bounds.origin + scroll_offset;
+            bounds.origin = bounds.origin + scroll_offset;
 
-        let inset = px(0.5);
-        let (selection, cursor) = if selected_range.is_empty() && input.show_cursor(cx) {
-            // cursor blink
-            (
-                None,
-                Some(fill(
+            if selected_range.is_empty() && input.show_cursor(cx) {
+                // cursor blink
+                cursor = Some(fill(
                     Bounds::new(
-                        point(bounds.left() + cursor_pos, bounds.top() + inset),
-                        size(px(2.), bounds.bottom() - bounds.top() - inset * 2),
+                        point(bounds.left() + cursor_pos, bounds.top() + INSET),
+                        size(px(2.), bounds.bottom() - bounds.top() - INSET * 2),
                     ),
                     crate::blue_500(),
-                )),
-            )
-        } else {
-            // selection background
-            (
-                Some(fill(
+                ))
+            } else {
+                // selection background
+                selection = Some(fill(
                     Bounds::from_corners(
                         point(
                             bounds.left() + line.x_for_index(selected_range.start),
@@ -1045,15 +1071,15 @@ impl Element for TextElement {
                         ),
                     ),
                     cx.theme().selection,
-                )),
-                None,
-            )
-        };
+                ))
+            };
+        }
 
         PrepaintState {
             scroll_offset,
             bounds,
-            line: Some(line),
+            line,
+            lines,
             cursor,
             selection,
         }
@@ -1080,8 +1106,25 @@ impl Element for TextElement {
         if let Some(selection) = prepaint.selection.take() {
             cx.paint_quad(selection)
         }
-        let line = prepaint.line.take().unwrap();
-        line.paint(bounds.origin, cx.line_height(), cx).unwrap();
+
+        let mut last_layout = None;
+
+        // Paint single line text
+        if let Some(line) = prepaint.line.take() {
+            line.paint(bounds.origin, cx.line_height(), cx).unwrap();
+            last_layout = Some(line);
+        }
+
+        // Paint multi line text
+        if let Some(lines) = prepaint.lines.take() {
+            let line_height = cx.line_height();
+            let origin = bounds.origin;
+
+            for (i, line) in lines.into_iter().enumerate() {
+                let p = point(origin.x, origin.y + i as f32 * line_height);
+                line.paint(p, line_height, cx).unwrap();
+            }
+        }
 
         if focused {
             if let Some(cursor) = prepaint.cursor.take() {
@@ -1090,7 +1133,8 @@ impl Element for TextElement {
         }
         self.input.update(cx, |input, _cx| {
             input.scroll_offset = prepaint.scroll_offset;
-            input.last_layout = Some(line);
+            // FIXME: To support multi-line text, we need to store the last layout for each line.
+            input.last_layout = last_layout;
             input.last_bounds = Some(bounds);
             input.last_cursor_offset = Some(cursor);
             input.last_selected_range = Some(selected_range);
@@ -1102,6 +1146,7 @@ impl Element for TextElement {
 
 impl Render for TextInput {
     fn render(&mut self, cx: &mut ViewContext<Self>) -> impl IntoElement {
+        const LINE_HEIGHT: Rems = Rems(1.25);
         let focused = self.focus_handle.is_focused(cx);
 
         let prefix = self.prefix.as_ref().map(|build| build(cx));
@@ -1142,10 +1187,11 @@ impl Render for TextInput {
             // .on_mouse_up_out(MouseButton::Left, cx.listener(Self::on_mouse_up))
             // .on_mouse_move(cx.listener(Self::on_mouse_move))
             .size_full()
-            .line_height(rems(1.25))
-            .text_size(rems(0.875))
+            .line_height(LINE_HEIGHT)
+            // .text_size(rems(0.875))
             .input_py(self.size)
             .input_h(self.size)
+            .when(self.multi_line, |this| this.h_auto())
             .when(self.appearance, |this| {
                 this.bg(if self.disabled {
                     cx.theme().muted
